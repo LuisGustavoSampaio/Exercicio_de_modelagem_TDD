c8b799a08e119b317cc4fa2860ab10e3ffd2a3f0
Luís Gustavo <luisgustavo120418@gmail.com>
2026-04-27 13:47:03 -0300

    Update teste.py

--- tests/teste.py
+++ tests/teste.py
@@ -2,26 +2,24 @@ import os
 import sys
 import unittest
 
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from model import Cliente, ContaPoupanca, InvestimentoPoupanca
+from model import Cliente, ContaPoupanca
+
 
 class TestInvestimentoPoupanca(unittest.TestCase):
-    def test_deve_aplicar_rendimento_ao_investir_na_poupanca(self):
+    def test_deve_aplicar_rendimento_na_poupanca(self):
         # Arrange
         cliente = Cliente("Luis", "12345678900", "01/01/2000", "Salvador")
-        conta_poupanca = ContaPoupanca(cliente, numero=1, taxa_rendimento=0.01)
+        conta_poupanca = ContaPoupanca(cliente, numero=1)
         conta_poupanca.depositar(1000)
-        investimento = InvestimentoPoupanca()
 
         # Act
-        investimento.registrar(conta_poupanca)
+        rendimento = conta_poupanca.aplicar_rendimento()
 
         # Assert
-        self.assertEqual(conta_poupanca.saldo, 1010)
-        self.assertEqual(len(conta_poupanca.historico.transacoes), 1)
-        self.assertIs(conta_poupanca.historico.transacoes[0], investimento)
+        self.assertAlmostEqual(rendimento, 5)
+        self.assertAlmostEqual(conta_poupanca.saldo, 1005)
 
 
 if __name__ == "__main__":
